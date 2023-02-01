21d6aecc7207e3602df5ef4ebc213158601763cb
minerva421 <minerva421.ai@gmail.com>
2023-02-01 12:43:38 +0900

    Events 2

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,6 +1,8 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");  
 
+const link = document.querySelector("a")
+
 function onLoginSumit(event) {
     event.preventDefault();
     console.log(event);
@@ -8,4 +10,11 @@ function onLoginSumit(event) {
     console.log(username);
 }
 
+function onClickLink(event) {
+    event.preventDefault();
+    console.dir(event)
+    console.log("링크 클릭")
+}
+
 loginForm.addEventListener("submit", onLoginSumit);  
+link.addEventListener("click", onClickLink)
