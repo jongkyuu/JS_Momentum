e5777faffde8671539cea8b7629132380704106c
minerva421 <minerva421.ai@gmail.com>
2023-02-14 12:52:36 +0900

    시계 추가

--- tutorial/js/clock.js
+++ tutorial/js/clock.js
@@ -1,2 +1,24 @@
 const clock = document.querySelector("h2#clock")
-clock.innerText = "test"
+// clock.innerText = "test"
+
+function sayHello() {
+    console.log("Hello");
+}
+
+function getClock() {
+    const date = new Date();
+    const hours = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+    const seconds = date.getSeconds().toString().padStart(2, "0");
+
+    const time = `${hours}:${minutes}:${seconds}`;
+    clock.innerText = time;
+}
+
+// setInterval(() => {
+//    sayHello(); 
+// }, 5000);
+
+// setInterval(sayHello, 1000);
+getClock();
+setInterval(getClock, 1000);
