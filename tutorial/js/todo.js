df4aeb58a5cd083b47b1457b34e433764a56227f
minerva421 <minerva421.ai@gmail.com>
2023-02-21 01:52:19 +0900

    todo List 불러오기 - mine

--- tutorial/js/todo.js
+++ tutorial/js/todo.js
@@ -2,11 +2,31 @@ const  toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "toDos";
+
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
     console.dir(event.target);
-    console.log(event.target.parentNode) ;
+    console.dir(event.target.parentNode);
+    console.log(event.target.parentNode.firstChild.innerText)
     const li = event.target.parentNode;
     li.remove();
+
+    const deletedTodo = event.target.parentNode.firstChild.innerText;
+    const parsedTodo = JSON.parse(localStorage.getItem(TODOS_KEY));
+    var deletedIndex = parsedTodo.indexOf(deletedTodo);
+    console.log(`deletedIndex : ${deletedIndex}`)
+    if (deletedIndex > -1) { 
+        parsedTodo.splice(deletedIndex, 1);
+    }
+    console.log(`parsedTodo : ${parsedTodo}`);
+
+    localStorage.setItem(TODOS_KEY, JSON.stringify(parsedTodo));
 }
 
 function paintTodo(newTodo) {
@@ -15,7 +35,7 @@ function paintTodo(newTodo) {
     span.innerText = newTodo;
 
     const button = document.createElement("button");
-    button.innerText = "X";
+    button.innerText = "❌";
     button.addEventListener("click", deleteToDo);
     li.appendChild(span);
     li.appendChild(button);
@@ -26,9 +46,22 @@ function handleToDoSumit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintTodo(newTodo);
+    saveToDos();
 }
 
-
+function sayHello(name) {
+    console.log(`say Hello ${name}`)
+}
 
 toDoForm.addEventListener("submit", handleToDoSumit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.forEach(toDo => {
+        paintTodo(toDo);
+    });
+}
