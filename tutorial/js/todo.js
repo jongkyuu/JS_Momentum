344b3d7852e083d4aaf36a4339c9751e5e5cd195
minerva421 <minerva421.ai@gmail.com>
2023-02-20 12:45:28 +0900

    todo list 삭제 추가

--- tutorial/js/todo.js
+++ tutorial/js/todo.js
@@ -2,6 +2,26 @@ const  toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+    console.dir(event.target);
+    console.log(event.target.parentNode) ;
+    const li = event.target.parentNode;
+    li.remove();
+}
+
+function paintTodo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newTodo;
+
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
 function handleToDoSumit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
@@ -9,12 +29,6 @@ function handleToDoSumit(event) {
     paintTodo(newTodo);
 }
 
-function paintTodo(newTodo) {
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-    li.appendChild(span);
-    span.innerText = newTodo;
-    toDoList.appendChild(li);
-}
+
 
 toDoForm.addEventListener("submit", handleToDoSumit);
